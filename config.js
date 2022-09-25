83092cc650369e10784c52f2a8643921ef9a7529
XIAO SAN <106890463+AMBYSBOTZ@users.noreply.github.com>
2022-09-25 19:01:35 +0700

    config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@
 //Dalam file _menunya.js
 
 //『 Utama 』 //
-global.owner = ['6282xxxxxx']  // isi jika perlu
-global.mods = ['6282xxxxxx']   // isi jika perlu
-global.prems = ['6282xxxxxx']  // isi jika perlu
+global.owner = ['6285798267620']  // isi jika perlu
+global.mods = ['6285798267620']   // isi jika perlu
+global.prems = ['6285798267620']  // isi jika perlu
 
 //Isi dengan nama dan nomor lu//
-global.nameowner = '⫹⫺ OWNER' // isi nama kalian bebas
-global.numberowner = '6282xxxxxx' // isi nomor kalian dengan awalan seperti di contoh 
-global.dana = '6282xxxxxx' // isi dengan nomor dana yg kamu punya 
-global.pulsa = '6282xxxxxx' // isi dengan nomor pulsa hp kalian
-global.gopay = '6282xxxxxx'
+global.nameowner = '⫹⫺ XIAO SAN ⫹⫺' // isi nama kalian bebas
+global.numberowner = '6285798267620' // isi nomor kalian dengan awalan seperti di contoh 
+global.dana = 'NOT FOUND' // isi dengan nomor dana yg kamu punya 
+global.pulsa = '085798267620' // isi dengan nomor pulsa hp kalian
+global.gopay = 'NOT FOUND'
 
 //Name Bot & Grouplink//
 global.namebot = 'R-txzyMd'
